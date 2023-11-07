2049a0e9ea9809e78d86885bd26269c767acc175
Wilsley Germano <wilsleygermano@gmail.com>
2023-11-07 15:02:30 -0300

    Sets theme up

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:tab_newz/theme/app_colors.dart';
+
+class AppTheme {
+  ThemeData light = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.light,
+      seedColor: AppColors.primaryLight,
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    fontFamily: 'Montserrat',
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w900,
+        color: AppColors.primaryDark,
+      ),
+      displayMedium: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w900,
+        color: AppColors.primaryDark,
+      ),
+      displaySmall: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w900,
+        color: AppColors.primaryDark,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primaryDark,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primaryDark,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primaryDark,
+      ),
+    ),
+  );
+
+  ThemeData dark = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: AppColors.primaryDark,
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    fontFamily: 'Montserrat',
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w900,
+        color: AppColors.primaryLight,
+      ),
+      displayMedium: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w900,
+        color: AppColors.primaryLight,
+      ),
+      displaySmall: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w900,
+        color: AppColors.primaryLight,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primaryLight,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primaryLight,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primaryLight,
+      ),
+    ),
+  );
+}
